b8f41aced3f5e8026a8bbd3ea6f5bbf642a49a94
Mahir Molai <mahir@molai.dev>
2022-08-07 18:34:54 +0500

    Move to TypeScript

--- index.js
+++ index.js
@@ -1,70 +1,79 @@
 #!/usr/bin/env node
-
+"use strict";
+exports.__esModule = true;
 /**
  * Syntax: No Tabs Or Spaces Other Than Between Keywords.
  */
-const fs = require("fs");
-
-class Compiler {
-    constructor(file) {
+var fs = require("fs");
+var Compiler = /** @class */ (function () {
+    function Compiler(file) {
         this.code = fs.readFileSync(file, "utf-8").split("\n");
     }
-
-    compile() {
-        const output = [];
-        const variables = {};
-
-        for (const line of this.code) {
-            switch(true) {
+    Compiler.prototype.compile = function () {
+        var output = [];
+        var variables = {};
+        for (var _i = 0, _a = this.code; _i < _a.length; _i++) {
+            var line = _a[_i];
+            switch (true) {
                 case line.startsWith("//"):
                     break;
-                case line.startsWith("print"): 
-                    if(line.includes('print: "')) {
-                        const printLine = line.trim().slice(8, -1);
+                case line.startsWith("print"):
+                    if (line.includes('print: "')) {
+                        var printLine = line.trim().slice(8, -1);
                         output.push(printLine);
-                    } else if(line.includes("operation")) {
-                        const printLine = line.trim().slice(7).split(" ");
-                        const numbers = printLine.slice(1);
-                        if(!numbers.includes("+") && !numbers.includes("-") && !numbers.includes("*") && !numbers.includes("/")) {
+                    }
+                    else if (line.includes("operation")) {
+                        var printLine = line.trim().slice(7).split(" ");
+                        var numbers = printLine.slice(1);
+                        if (!numbers.includes("+") && !numbers.includes("-") && !numbers.includes("*") && !numbers.includes("/")) {
                             output.push("Incorrect Syntax");
-                        } else if(numbers[1] === "+") {
-                            output.push(Number(numbers[0]) + Number(numbers[2]))
-                        } else if(numbers[1] === "-") {
-                            output.push(Number(numbers[0]) - Number(numbers[2]))
-                        } else if(numbers[1] === "*") {
-                            output.push(Number(numbers[0]) * Number(numbers[2]))
-                        } else if(numbers[1] === "/") {
-                            output.push(Number(numbers[0]) / Number(numbers[2]))
                         }
-                    } else {
-                        const printLine = line.trim().slice(7);
-                        const variableValue = variables[printLine];
-                        if(!variableValue.includes("operation")) {
+                        else if (numbers[1] === "+") {
+                            output.push(Number(numbers[0]) + Number(numbers[2]));
+                        }
+                        else if (numbers[1] === "-") {
+                            output.push(Number(numbers[0]) - Number(numbers[2]));
+                        }
+                        else if (numbers[1] === "*") {
+                            output.push(Number(numbers[0]) * Number(numbers[2]));
+                        }
+                        else if (numbers[1] === "/") {
+                            output.push(Number(numbers[0]) / Number(numbers[2]));
+                        }
+                    }
+                    else {
+                        var printLine = line.trim().slice(7);
+                        var variableValue = variables[printLine];
+                        if (!variableValue.includes("operation")) {
                             output.push(variableValue.slice(1, -1));
-                        } else if(variableValue.includes("operation")) {
-                            const numbers = variableValue.split(" ").slice(1);
-                            if(!numbers.includes("+") && !numbers.includes("-") && !numbers.includes("*") && !numbers.includes("/")) {
+                        }
+                        else if (variableValue.includes("operation")) {
+                            var numbers = variableValue.split(" ").slice(1);
+                            if (!numbers.includes("+") && !numbers.includes("-") && !numbers.includes("*") && !numbers.includes("/")) {
                                 output.push("Incorrect Syntax");
-                            } else if(numbers[1] === "+") {
-                                output.push(Number(numbers[0]) + Number(numbers[2]))
-                            } else if(numbers[1] === "-") {
-                                output.push(Number(numbers[0]) - Number(numbers[2]))
-                            } else if(numbers[1] === "*") {
-                                output.push(Number(numbers[0]) * Number(numbers[2]))
-                            } else if(numbers[1] === "/") {
-                                output.push(Number(numbers[0]) / Number(numbers[2]))
+                            }
+                            else if (numbers[1] === "+") {
+                                output.push(Number(numbers[0]) + Number(numbers[2]));
+                            }
+                            else if (numbers[1] === "-") {
+                                output.push(Number(numbers[0]) - Number(numbers[2]));
+                            }
+                            else if (numbers[1] === "*") {
+                                output.push(Number(numbers[0]) * Number(numbers[2]));
+                            }
+                            else if (numbers[1] === "/") {
+                                output.push(Number(numbers[0]) / Number(numbers[2]));
                             }
                         }
                     }
-
                     break;
                 case line.startsWith("operation"):
-                    const line2 = line.slice(5).split(" ");
+                    var line2 = line.slice(5).split(" ");
                     console.log(line2);
                     break;
-                case line.startsWith("def"):  
-                    const defArray = line.slice(5).split(" = ");
-                    const variableName = defArray[0];
+                case line.startsWith("def"):
+                    var defArray = line.slice(5).split(" = ");
+                    var variableName = defArray[0];
                     variables[variableName] = defArray[1];
                     /**
                      * console.log('VARS:');
@@ -74,20 +83,16 @@ class Compiler {
                 case !line.startsWith("print"):
                     output.push("Error: Invalid syntax.");
                     break;
-                
             }
         }
-
         return output;
-    }
-
-    run() {
-        const output = this.compile();
+    };
+    Compiler.prototype.run = function () {
+        var output = this.compile();
         console.log(output.join("\n"));
-    }
-}
-
-const filePath = process.argv.slice(2);
-
-const compiler = new Compiler(filePath[0]);
+    };
+    return Compiler;
+}());
+var filePath = process.argv.slice(2);
+var compiler = new Compiler(filePath[0]);
 compiler.run();
---
+++ index.ts
@@ -0,0 +1,94 @@
+#!/usr/bin/env node
+
+/**
+ * Syntax: No Tabs Or Spaces Other Than Between Keywords.
+ */
+import * as fs from "fs";
+
+class Compiler {
+    code: any;
+    constructor(file) {
+        this.code = fs.readFileSync(file, "utf-8").split("\n");
+    }
+
+    compile() {
+        const output: any = [];
+        const variables = {};
+
+        for (const line of this.code) {
+            switch(true) {
+                case line.startsWith("//"):
+                    break;
+                case line.startsWith("print"): 
+                    if(line.includes('print: "')) {
+                        const printLine: any = line.trim().slice(8, -1);
+                        output.push(printLine);
+                    } else if(line.includes("operation")) {
+                        const printLine: any = line.trim().slice(7).split(" ");
+                        const numbers: any = printLine.slice(1);
+                        if(!numbers.includes("+") && !numbers.includes("-") && !numbers.includes("*") && !numbers.includes("/")) {
+                            output.push("Incorrect Syntax");
+                        } else if(numbers[1] === "+") {
+                            output.push(Number(numbers[0]) + Number(numbers[2]))
+                        } else if(numbers[1] === "-") {
+                            output.push(Number(numbers[0]) - Number(numbers[2]))
+                        } else if(numbers[1] === "*") {
+                            output.push(Number(numbers[0]) * Number(numbers[2]))
+                        } else if(numbers[1] === "/") {
+                            output.push(Number(numbers[0]) / Number(numbers[2]))
+                        }
+                    } else {
+                        const printLine = line.trim().slice(7);
+                        const variableValue = variables[printLine];
+                        if(!variableValue.includes("operation")) {
+                            output.push(variableValue.slice(1, -1));
+                        } else if(variableValue.includes("operation")) {
+                            const numbers = variableValue.split(" ").slice(1);
+                            if(!numbers.includes("+") && !numbers.includes("-") && !numbers.includes("*") && !numbers.includes("/")) {
+                                output.push("Incorrect Syntax");
+                            } else if(numbers[1] === "+") {
+                                output.push(Number(numbers[0]) + Number(numbers[2]))
+                            } else if(numbers[1] === "-") {
+                                output.push(Number(numbers[0]) - Number(numbers[2]))
+                            } else if(numbers[1] === "*") {
+                                output.push(Number(numbers[0]) * Number(numbers[2]))
+                            } else if(numbers[1] === "/") {
+                                output.push(Number(numbers[0]) / Number(numbers[2]))
+                            }
+                        }
+                    }
+
+                    break;
+                case line.startsWith("operation"):
+                    const line2 = line.slice(5).split(" ");
+                    console.log(line2);
+                    break;
+                case line.startsWith("def"):  
+                    const defArray = line.slice(5).split(" = ");
+                    const variableName = defArray[0];
+                    variables[variableName] = defArray[1];
+                    /**
+                     * console.log('VARS:');
+                     * console.log(variables);
+                     */
+                    break;
+                case !line.startsWith("print"):
+                    output.push("Error: Invalid syntax.");
+                    break;
+                
+            }
+        }
+
+        return output;
+    }
+
+    run() {
+        const output = this.compile();
+        console.log(output.join("\n"));
+    }
+}
+
+const filePath = process.argv.slice(2);
+
+const compiler = new Compiler(filePath[0]);
+compiler.run();
